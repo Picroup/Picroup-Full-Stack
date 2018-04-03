64df31a46bac1a7650532c3080467f32221ddf3d
beeth0ven <beeth0vendev@gmail.com>
2018-04-03 20:39:03 +0800

    refactor test mutation followUser

--- tests/testcases/resolvers/Mutation/followUser.test.js
+++ tests/testcases/resolvers/Mutation/followUser.test.js
@@ -6,14 +6,10 @@ import ReputationKind from "../../../../server/usecases/model/ReputationKind";
 import FollowUserLink from "../../../../server/usecases/mongoose/FollowUserLink/index";
 import Notification from "../../../../server/usecases/mongoose/Notification/index";
 import NotificationKind from "../../../../server/usecases/model/NotificationKind";
+import ReputationLink from "../../../../server/usecases/mongoose/ReputationLink";
 
 let mongoService;
 let followUser;
-
-const clearData = async () => {
-  await User.remove({});
-  await FollowUserLink.remove({});
-};
 
 beforeAll(async () => {
   mongoService = new MongoTestService({});
@@ -27,57 +23,84 @@ afterAll(async () => {
 
 describe('Resolver Mutation followUser', () => {
 
-  it('should test followUser basic', async () => {
+  describe('followUser basic', async () => {
 
     const userIdKey = '5ab992fe05b6e9bf4c253b53';
     const toUserIdKey = '5abb34e67f9f4cf1429de9b0';
     const userId = new ObjectId(userIdKey);
     const toUserId = new ObjectId(toUserIdKey);
 
-    // initial state
-    await User.insertMany([
-      {_id: userId, username: 'luojie', password: '123' },
-      {_id: toUserId, username: 'li', password: '321' },
-    ]);
-
-    // perform operation
-    await followUser({}, {userId: userIdKey, toUserId: toUserIdKey});
-
-    // check result
-    const user = await User.findById(userId);
-    const toUser = await User.findById(toUserId);
-    const link = await FollowUserLink.findOne({userId, toUserId});
-    const notification = await Notification.findOne({userId, toUserId, kind: NotificationKind.followUser});
-
-    expect(user).toMatchObject({
-      _id: userId,
-      username: 'luojie',
-      followingsCount: 1,
-      followersCount: 0,
-      reputation: 0
+    beforeEach(async () => {
+      await User.insertMany([
+        {_id: userId, username: 'luojie', password: '123' },
+        {_id: toUserId, username: 'li', password: '321' },
+      ]);
     });
 
-    expect(toUser).toMatchObject({
-      _id: toUserId,
-      username: 'li',
-      followingsCount: 0,
-      followersCount: 1,
-      reputation: ReputationKind.reputationValue(ReputationKind.followUser)
+    afterEach(async () => {
+      await User.remove({});
+      await FollowUserLink.remove({});
+      await ReputationLink.remove({});
+      await Notification.remove({});
     });
 
-    expect(link).toMatchObject({
-      userId,
-      toUserId,
+    it('should test user', async () => {
+
+      await followUser({}, {userId: userIdKey, toUserId: toUserIdKey});
+      const user = await User.findById(userId);
+
+      expect(user).toMatchObject({
+        _id: userId,
+        username: 'luojie',
+        followingsCount: 1,
+        followersCount: 0,
+        reputation: 0
+      });
+
     });
 
-    expect(notification).toMatchObject({
-      userId,
-      toUserId,
-      kind: NotificationKind.followUser,
-      content: undefined,
-      viewed: false
+    it('should test toUser', async () => {
+
+      await followUser({}, {userId: userIdKey, toUserId: toUserIdKey});
+      const toUser = await User.findById(toUserId);
+
+      expect(toUser).toMatchObject({
+        _id: toUserId,
+        username: 'li',
+        followingsCount: 0,
+        followersCount: 1,
+        reputation: ReputationKind.reputationValue(ReputationKind.followUser)
+      });
+
     });
 
-    await clearData();
-  })
+    it('should test link', async () => {
+
+      await followUser({}, {userId: userIdKey, toUserId: toUserIdKey});
+      const link = await FollowUserLink.findOne({userId, toUserId});
+
+      expect(link).toMatchObject({
+        userId,
+        toUserId,
+      });
+
+    });
+
+    it('should test notification', async () => {
+
+      await followUser({}, {userId: userIdKey, toUserId: toUserIdKey});
+      const notification = await Notification.findOne({userId, toUserId, kind: NotificationKind.followUser});
+
+      expect(notification).toMatchObject({
+        userId,
+        toUserId,
+        kind: NotificationKind.followUser,
+        content: undefined,
+        viewed: false
+      });
+
+    });
+
+  });
+
 });
