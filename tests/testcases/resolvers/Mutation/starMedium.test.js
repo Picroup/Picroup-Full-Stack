753f2c9274fc5acc7337dccee0e2b7a517065d79
beeth0ven <beeth0vendev@gmail.com>
2018-04-03 21:13:49 +0800

    refactor test mutation starMedium

--- tests/testcases/resolvers/Mutation/starMedium.test.js
+++ tests/testcases/resolvers/Mutation/starMedium.test.js
@@ -14,13 +14,6 @@ import NotificationKind from "../../../../server/usecases/model/NotificationKind
 let mongoService;
 let starMedium;
 
-const clearData = async () => {
-  await User.remove({});
-  await Medium.remove({});
-  await StarMediumLink.remove({});
-  await ReputationLink.remove({});
-};
-
 beforeAll(async () => {
   mongoService = new MongoTestService({});
   await mongoService.start();
@@ -33,7 +26,7 @@ afterAll(async () => {
 
 describe('Resolver Mutation starMedium', () => {
 
-  it('should test starMedium basic', async () => {
+  describe('basic', async () => {
 
     const userIdKey = '5ab992fe05b6e9bf4c253b53';
     const toUserIdKey = '5abb34e67f9f4cf1429de9b0';
@@ -43,74 +36,112 @@ describe('Resolver Mutation starMedium', () => {
     const mediumId = new ObjectId(mediumIdKey);
     const endedAt = 0;
 
-    // initial state
-    await User.insertMany([
-      {_id: userId, username: 'luojie', password: '123'},
-      {_id: toUserId, username: 'li', password: '123'},
-    ]);
+    beforeEach(async () => {
+      await User.insertMany([
+        {_id: userId, username: 'luojie', password: '123'},
+        {_id: toUserId, username: 'li', password: '123'},
+      ]);
 
-    await Medium.create(
-      { _id: mediumId, userId: toUserId, minioId: 'minioId0', category : 'beauty', kind : 'image', endedAt }
-    );
-
-    // perform operation
-    await starMedium({}, {userId, mediumId});
-
-    // check result
-    const medium = await Medium.findById(mediumId);
-    const user = await User.findById(userId);
-    const toUser = await User.findById(toUserId);
-    const starMediumLink = await StarMediumLink.findOne({userId, mediumId});
-    const reputationLink = await ReputationLink.findStarMediumLink({userId, mediumId});
-    const notification = await Notification.findOne({userId, mediumId, kind: NotificationKind.starMedium});
-
-    expect(medium).toMatchObject({
-      _id: mediumId,
-      userId: toUserId,
-      minioId: 'minioId0',
-      category : 'beauty',
-      kind : 'image',
-      endedAt: oneWeek
+      await Medium.create(
+        { _id: mediumId, userId: toUserId, minioId: 'minioId0', category : 'beauty', kind : 'image', endedAt }
+      );
     });
 
-    expect(user).toMatchObject({
-      _id: userId,
-      username: 'luojie',
-      followingsCount: 0,
-      followersCount: 0,
-      reputation: 0,
+    afterEach(async () => {
+      await User.remove({});
+      await Medium.remove({});
+      await StarMediumLink.remove({});
+      await ReputationLink.remove({});
     });
 
-    expect(toUser).toMatchObject({
-      _id: toUserId,
-      username: 'li',
-      followingsCount: 0,
-      followersCount: 0,
-      reputation: ReputationKind.reputationValue(ReputationKind.starMedium),
+    it('should test medium', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const medium = await Medium.findById(mediumId);
+
+      expect(medium).toMatchObject({
+        _id: mediumId,
+        userId: toUserId,
+        minioId: 'minioId0',
+        category : 'beauty',
+        kind : 'image',
+        endedAt: endedAt + oneWeek
+      });
+
     });
 
-    expect(starMediumLink).toMatchObject({
-      userId,
-      mediumId,
+    it('should test user', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const user = await User.findById(userId);
+
+      expect(user).toMatchObject({
+        _id: userId,
+        username: 'luojie',
+        followingsCount: 0,
+        followersCount: 0,
+        reputation: 0,
+      });
+
     });
 
-    expect(reputationLink).toMatchObject({
-      userId,
-      mediumId,
-      toUserId,
-      kind: ReputationKind.starMedium,
+    it('should test toUser', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const toUser = await User.findById(toUserId);
+
+      expect(toUser).toMatchObject({
+        _id: toUserId,
+        username: 'li',
+        followingsCount: 0,
+        followersCount: 0,
+        reputation: ReputationKind.reputationValue(ReputationKind.starMedium),
+      });
+
     });
 
-    expect(notification).toMatchObject({
-      userId,
-      toUserId,
-      mediumId,
-      kind: NotificationKind.starMedium,
-      content: undefined,
-      viewed: false
+    it('should test starMediumLink', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const starMediumLink = await StarMediumLink.findOne({userId, mediumId});
+
+      expect(starMediumLink).toMatchObject({
+        userId,
+        mediumId,
+      });
+
     });
 
-    await clearData();
+    it('should test reputationLink', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const reputationLink = await ReputationLink.findStarMediumLink({userId, mediumId});
+
+      expect(reputationLink).toMatchObject({
+        userId,
+        mediumId,
+        toUserId,
+        kind: ReputationKind.starMedium,
+      });
+
+    });
+
+    it('should test notification', async () => {
+
+      await starMedium({}, {userId, mediumId});
+      const notification = await Notification.findOne({userId, mediumId, kind: NotificationKind.starMedium});
+
+      expect(notification).toMatchObject({
+        userId,
+        toUserId,
+        mediumId,
+        kind: NotificationKind.starMedium,
+        content: undefined,
+        viewed: false
+      });
+
+    });
+
   });
 
 });
