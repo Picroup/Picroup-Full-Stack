a36ce8dca42461dac53c7db950c69fbae2f35813
beeth0ven <beeth0vendev@gmail.com>
2018-05-21 20:53:04 +0800

    [feature] return toUser when follow user

--- server/graphql/resolvers/Mutation/followUser.js
+++ server/graphql/resolvers/Mutation/followUser.js
@@ -1,4 +1,20 @@
 
+const saveFollowUserLink = ({dependency: {
+  ReputationLink,
+  User,
+  Notification,
+}}) => async ({userId, toUserId}) => {
+  try {
+    const reputation = await ReputationLink.saveFollowUserLink({userId, toUserId});
+    await User.increaseReputation({userId: toUserId, reputation: reputation.value});
+    await User.increaseGainedReputation({userId: toUserId, reputation: reputation.value});
+    await Notification.saveFollowUserNotification({userId, toUserId});
+    await User.increaseNotificationsCount(toUserId);
+  } catch (error) {
+    console.error(error)
+  }
+};
+
 export const createFollowUserResolver = ({dependency: {
   FollowUserLink,
   User,
@@ -6,12 +22,8 @@ export const createFollowUserResolver = ({dependency: {
   Notification,
 }}) => async (_, {userId, toUserId}) => {
   await FollowUserLink.saveLink({userId, toUserId});
-  await User.increaseFollowersCount(toUserId);
-  const updatedUser = await User.increaseFollowingsCount(userId);
-  const reputation = await ReputationLink.saveFollowUserLink({userId, toUserId});
-  await User.increaseReputation({userId: toUserId, reputation: reputation.value});
-  await User.increaseGainedReputation({userId: toUserId, reputation: reputation.value});
-  await Notification.saveFollowUserNotification({userId, toUserId});
-  await User.increaseNotificationsCount(toUserId);
+  const updatedUser = await User.increaseFollowersCount(toUserId);
+  await User.increaseFollowingsCount(userId);
+  await saveFollowUserLink({dependency: {ReputationLink, User, Notification}})({userId, toUserId});
   return updatedUser;
 };
