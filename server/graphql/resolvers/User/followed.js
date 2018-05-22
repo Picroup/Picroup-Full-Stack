c66fc3f94ea73003974b2e670977d0b1d6d5eb65
beeth0ven <beeth0vendev@gmail.com>
2018-05-22 20:48:44 +0800

    fix follow myself retrun null instead of false

--- server/graphql/resolvers/User/followed.js
+++ server/graphql/resolvers/User/followed.js
@@ -2,7 +2,7 @@
 export const createFollowedResolver = ({dependency: {
   FollowUserLink
   }}) => async ({_id: toUserId}, { byUserId: userId }) => {
-  if (userId === toUserId) return null;
+  if (toUserId.equals(userId)) return null;
   return await FollowUserLink
     .find({userId, toUserId})
     .limit(1)
