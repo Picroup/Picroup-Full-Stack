baa5c53c9a87982d6e17c51b5ac44050ac9d938f
beeth0ven <beeth0vendev@163.com>
2018-05-30 16:46:14 +0800

    not add reputation when star self media

--- server/graphql/resolvers/Mutation/starMedium.js
+++ server/graphql/resolvers/Mutation/starMedium.js
@@ -10,10 +10,10 @@ export const createStarMediumResolver = ({dependency: {
   await StarMediumLink.saveLink({userId, mediumId});
   const updatedMedium = await Medium.increaseEndedAt({mediumId, duration: oneWeek});
   const toUserId = updatedMedium.userId;
+  if (toUserId.equals(userId)) return updatedMedium;
   const reputation = await ReputationLink.saveStarMediumLink({userId, mediumId, toUserId});
   await User.increaseReputation({userId: toUserId, reputation: reputation.value});
   await User.increaseGainedReputation({userId: toUserId, reputation: reputation.value});
-  if (toUserId.equals(userId)) return updatedMedium;
   await Notification.saveStartMediumNotification({userId, toUserId, mediumId});
   await User.increaseNotificationsCount(toUserId);
   return updatedMedium
