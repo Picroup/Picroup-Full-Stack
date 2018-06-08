24c3359a5ef7f70dce81e809d1b38ea5042fd723
beeth0ven <beeth0vendev@gmail.com>
2018-06-08 18:23:07 +0800

    random 0.1 to 1.0

--- server/graphql/resolvers/Query/hotMedia.js
+++ server/graphql/resolvers/Query/hotMedia.js
@@ -1,14 +1,15 @@
 import {PAGE_LIMIT} from "../../../config";
 import {getCurrentTimestamp} from "../../../libraries/date";
+import {random} from "../../../libraries/math";
 
 export const createHotMediaResolver = ({dependency: {
   Medium
 }}) => async () => {
 
   const { endedAt } = await Medium.findOne().sort({endedAt: -1});
-  const randomHalfToOne = 0.5 * Math.random() + 0.5;
+  const randomValue = random({min: 0.1, max: 1.0});
   const now = getCurrentTimestamp();
-  const maxEndedAt = now + (endedAt - now) * randomHalfToOne;
+  const maxEndedAt = now + (endedAt - now) * randomValue;
 
   const items = await Medium
     .find({ endedAt: {$lte: maxEndedAt} })
